492d1092d8cdebd2d27efd1cda455323151fc2b4
Dustin Lang <dstndstn@gmail.com>
2015-11-28 01:25:22 -0300

    read existing plan file, read tiles file, call ExposureFactor

--- measure_raw_decam.py
+++ measure_raw_decam.py
@@ -6,6 +6,7 @@ import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 import pylab as plt
+import json
 
 from scipy.stats import sigmaclip
 from scipy.ndimage.filters import gaussian_filter
@@ -13,14 +14,18 @@ from scipy.ndimage.measurements import label, find_objects, center_of_mass
 from scipy.ndimage.filters import median_filter
 
 from astrometry.util.plotutils import *
+from astrometry.util.fits import *
 from astrometry.util.util import wcs_pv2sip_hdr
 from astrometry.libkd.spherematch import match_xy
+
+import ephem
 
 from legacyanalysis.ps1cat import ps1cat, ps1_to_decam
 
 import photutils
 
-from nightlystrategy import ExposureFactor
+from nightlystrategy import (ExposureFactor, getParserAndGlobals, setupGlobals,
+                             GetAirmass, StartAndEndTimes, s_to_days)
 
 def sensible_sigmaclip(arr, nsigma = 4.):
     goodpix,lo,hi = sigmaclip(arr, low=nsigma, high=nsigma)
@@ -516,7 +521,7 @@ def measure_raw_decam(fn, ext='N4'):
                                   tractor.Flux(fluxi))
         tr = tractor.Tractor([tim],[src])
 
-        doplot = (i < 20)
+        doplot = (i < 5)
         if doplot:
             mod0 = tr.getModelImage(0)
 
@@ -597,6 +602,86 @@ def measure_raw_decam(fn, ext='N4'):
         plt.plot(xx, yy, 'k-', alpha=0.1)
     plt.ylim(-0.2, 1.0)
     ps.savefig()
-        
-measure_raw_decam('DECam_00488199.fits.fz')
-    
+
+
+    return dict(band=band, airmass=airmass, seeing=fwhm, zp=zp_obs,
+                skybright=skybr, transparency=transparency)
+
+
+
+jsonfn = 'decals_2015-11-27_plan.json'
+J = json.loads(open(jsonfn,'rb').read())
+
+tiles = fits_table('decam-tiles_obstatus.fits')
+
+date = '2015-11-17'
+passnum = 1
+
+j = J[0]
+print('Planned tile:', j)
+
+#M = measure_raw_decam('DECam_00488199.fits.fz')
+M = {'seeing': 1.4890481099577366, 'airmass': 1.34,
+     'skybright': 18.383479116033314, 'transparency': 0.94488537276869045,
+     'band': 'z', 'zp': 26.442847814941093}
+
+print('Measurements:', M)
+
+#class Duck(object):
+#    pass
+
+parser,gvs = getParserAndGlobals()
+# opt = Duck()
+# opt.date = date
+# opt.passnumber = passnum
+# opt.portion = 1.0
+# opt.start_date = '0000-00-00'
+# opt.start_time = 
+
+opt,args = parser.parse_args(('--date %s --pass %i --portion 1.0' %
+                              (date, passnum)).split())
+
+obs = setupGlobals(opt, gvs)
+
+gvs.transparency = M['transparency']
+
+objname = j['object']
+# Parse objname like 'DECaLS_5623_z'
+parts = objname.split('_')
+assert(len(parts) == 3)
+assert(parts[0] == 'DECaLS')
+nextband = parts[2]
+assert(nextband in 'grz')
+tilenum = int(parts[1])
+
+print('Next planned tile:', tilenum, nextband)
+
+# Set current date
+part = 1.0
+(sn, en, lon, sn_18, en_18) = StartAndEndTimes(obs, part, gvs)
+time_elapsed = 0.
+obs.date = sn + time_elapsed*s_to_days
+
+present_tile = ephem.readdb(str(objname+','+'f'+','+('%.6f' % j['RA'])+','+
+                                ('%.6f' % j['dec'])+','+'20'))
+present_tile.compute(obs)
+airmass = GetAirmass(float(present_tile.alt))
+print('Airmass of planned tile:', airmass)
+
+# Find this tile in the tiles table.
+tile = tiles[tilenum-1]
+if tile.tileid != tilenum:
+    I = np.flatnonzero(tile.tileid == tilename)
+    assert(len(I) == 1)
+    tile = tiles[I[0]]
+
+ebv = tile.ebv_med
+print('E(b-v) of planned tile:', ebv)
+
+fakezp = -99
+expfactor = ExposureFactor(M['band'], airmass, ebv, M['seeing'], fakezp,
+                           M['skybright'], gvs)
+print('Exposure factor:', expfactor)
+
+
+
