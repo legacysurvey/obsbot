e5c850bdd85349fcd85f56bd0fa850fb5aa3c717
Dustin Lang <dstndstn@gmail.com>
2016-01-09 09:21:46 -0500

    factor out the function we run on each image; allow specifying a lits of images on the command line

--- copilot.py
+++ copilot.py
@@ -38,6 +38,105 @@ from measure_raw_decam import measure_raw_decam
 
 from tractor.sfd import SFDMap
 
+def process_image(fn, ext, gvs, sfd, opt):
+    portion = opt.portion
+
+    # Read primary FITS header
+    phdr = fitsio.read_header(fn)
+    # Write QA plots to files named by the exposure number
+    expnum = phdr['EXPNUM']
+    ps = PlotSequence('qa-%i' % expnum)
+    # Measure the new image
+    M = measure_raw_decam(fn, ext=ext, ps=ps)
+
+    # (example results for testig)
+    #M = {'seeing': 1.4890481099577366, 'airmass': 1.34,
+    #'skybright': 18.383479116033314, 'transparency': 0.94488537276869045,
+    #'band': 'z', 'zp': 26.442847814941093}
+
+    print('Measurements:', M)
+
+    gvs.transparency = M['transparency']
+    band = M['band']
+
+    ra  = hmsstring2ra (phdr['RA'])
+    dec = dmsstring2dec(phdr['DEC'])
+    airmass = phdr['AIRMASS']
+    actual_exptime = phdr['EXPTIME']
+
+    # Look up E(B-V) in SFD map
+    ebv = sfd.ebv(ra, dec)[0]
+    print('E(B-V):', ebv)
+
+    fakezp = -99
+    expfactor = ExposureFactor(band, airmass, ebv, M['seeing'], fakezp,
+                               M['skybright'], gvs)
+    print('Exposure factor:', expfactor)
+    exptime = expfactor * gvs.base_exptimes[band]
+    print('Exptime (un-clipped)', exptime)
+    exptime = np.clip(exptime, gvs.floor_exptimes[band], gvs.ceil_exptimes[band])
+    print('Clipped exptime', exptime)
+    
+    if band == 'z' and exptime > gvs.t_sat_max:
+        exptime = gvs.t_sat_max
+        print('Reduced exposure time to avoid z-band saturation:', exptime)
+
+    print
+
+    print('Actual exposure time taken:', actual_exptime)
+
+    print('Depth fraction: %6.3f' % (actual_exptime / exptime))
+    
+    # If you were going to re-plan, you would run with these args:
+    plandict = dict(seeing=M['seeing'], transparency=M['transparency'])
+    # Assume the sky is as much brighter than canonical in each band... unlikely
+    dsky = M['skybright'] - gvs.sb_dict[M['band']]
+    for band in 'grz':
+        plandict['sb'+band] = gvs.sb_dict[band] + dsky
+    # Note that nightlystrategy.py takes UTC dates.
+    now = datetime.datetime.utcnow()
+    d = now.date()
+    plandict['startdate'] = '%04i-%02i-%02i' % (d.year, d.month, d.day)
+    t = now.time()
+    plandict['starttime'] = t.strftime('%H:%M:%S')
+    # Make an hour-long plan
+    end = now + datetime.timedelta(0, 3600)
+    d = end.date()
+    plandict['enddate'] = '%04i-%02i-%02i' % (d.year, d.month, d.day)
+    t = end.time()
+    plandict['endtime'] = t.strftime('%H:%M:%S')
+
+    # Decide the pass.
+    goodseeing = plandict['seeing']<1.3
+    photometric = plandict['transparency']>0.95
+
+    if goodseeing and photometric:
+        passnum = 1
+    elif goodseeing or photometric:
+        passunm = 2
+    else:
+        passnum = 3
+    plandict['pass'] = passnum
+
+    plandict['portion'] = portion
+    
+    print('Replan command:')
+    print()
+    print('python2.7 nightlystrategy.py --seeg %(seeing).3f --seer %(seeing).3f --seez %(seeing).3f --sbg %(sbg).3f --sbr %(sbr).3f --sbz %(sbz).3f --transparency %(transparency).3f --start-date %(startdate)s --start-time %(starttime)s --end-date %(enddate)s --end-time %(endtime)s --date %(startdate)s --portion %(portion)f --pass %(pass)i' % plandict) 
+    print()
+
+    # Gather all the QAplots into a single pdf.
+    qafile = 'qa-%i.pdf' % expnum
+    pnglist = sorted(glob('qa-%i-??.png' % expnum))
+    cmd = 'convert {} {}'.format(' '.join(pnglist), qafile)
+    print('Writing out {}'.format(qafile))
+    print(cmd)
+    os.system(cmd)
+    [os.remove(png) for png in pnglist]
+
+    return M, plandict, expnum
+    
+    
 if __name__ == '__main__':
     
     parser = optparse.OptionParser(usage='%prog')
@@ -45,26 +144,29 @@ if __name__ == '__main__':
     parser.add_option('--ext', help='Extension to read for computing observing conditions: default %default', default='N4')
     parser.add_option('--rawdata', help='Directory to monitor for new images: default %default', default='rawdata')
     parser.add_option('--portion', help='Portion of the night: default %default', type=float, default='1.0')
-    
+
     opt,args = parser.parse_args()
-    if len(args) != 0:
-        parser.print_help()
-        sys.exit(-1)
-    
+
     imagedir = opt.rawdata
     rawext = opt.ext
-    portion = opt.portion
-    
+
     # Get nightlystrategy data structures; use fake command-line args.
     # these don't matter at all, since we only use ExposureFactor
     parser,gvs = getParserAndGlobals()
-    opt,args = parser.parse_args('--date 2015-01-01 --pass 1 --portion 1'.split())
-    obs = setupGlobals(opt, gvs)
-    
-    lastimages = set(os.listdir(imagedir))
-    
+    nsopt,nsargs = parser.parse_args('--date 2015-01-01 --pass 1 --portion 1'.split())
+    obs = setupGlobals(nsopt, gvs)
+
     print('Loading SFD maps...')
     sfd = SFDMap()
+    
+    if len(args) > 0:
+        for fn in args:
+            print('Reading', fn)
+            process_image(fn, rawext, gvs, sfd, opt)
+        sys.exit(0)
+    
+    
+    lastimages = set(os.listdir(imagedir))
     
     print('Checking directory for new files:', imagedir)
     while True:
@@ -109,97 +211,6 @@ if __name__ == '__main__':
             images.add(newestimg)
             lastimages = images
             break
-    
-        # Read primary FITS header
-        phdr = fitsio.read_header(fn)
-        # Write QA plots to files named by the exposure number
-        expnum = phdr['EXPNUM']
-        ps = PlotSequence('qa-%i' % expnum)
-        # Measure the new image
-        M = measure_raw_decam(fn, ext=rawext, ps=ps)
-    
-        # (example results for testig)
-        #M = {'seeing': 1.4890481099577366, 'airmass': 1.34,
-        #'skybright': 18.383479116033314, 'transparency': 0.94488537276869045,
-        #'band': 'z', 'zp': 26.442847814941093}
-    
-        print('Measurements:', M)
-    
-        gvs.transparency = M['transparency']
-        band = M['band']
-    
-        ra  = hmsstring2ra (phdr['RA'])
-        dec = dmsstring2dec(phdr['DEC'])
-        airmass = phdr['AIRMASS']
-        actual_exptime = phdr['EXPTIME']
-    
-        # Look up E(B-V) in SFD map
-        ebv = sfd.ebv(ra, dec)[0]
-        print('E(B-V):', ebv)
-    
-        fakezp = -99
-        expfactor = ExposureFactor(band, airmass, ebv, M['seeing'], fakezp,
-                                   M['skybright'], gvs)
-        print('Exposure factor:', expfactor)
-        exptime = expfactor * gvs.base_exptimes[band]
-        print('Exptime (un-clipped)', exptime)
-        exptime = np.clip(exptime, gvs.floor_exptimes[band], gvs.ceil_exptimes[band])
-        print('Clipped exptime', exptime)
-        
-        if band == 'z' and exptime > gvs.t_sat_max:
-            exptime = gvs.t_sat_max
-            print('Reduced exposure time to avoid z-band saturation:', exptime)
-    
-        print
-    
-        print('Actual exposure time taken:', actual_exptime)
-    
-        print('Depth fraction: %6.3f' % (actual_exptime / exptime))
-        
-        # If you were going to re-plan, you would run with these args:
-        plandict = dict(seeing=M['seeing'], transparency=M['transparency'])
-        # Assume the sky is as much brighter than canonical in each band... unlikely
-        dsky = M['skybright'] - gvs.sb_dict[M['band']]
-        for band in 'grz':
-            plandict['sb'+band] = gvs.sb_dict[band] + dsky
-        # Note that nightlystrategy.py takes UTC dates.
-        now = datetime.datetime.utcnow()
-        d = now.date()
-        plandict['startdate'] = '%04i-%02i-%02i' % (d.year, d.month, d.day)
-        t = now.time()
-        plandict['starttime'] = t.strftime('%H:%M:%S')
-        # Make an hour-long plan
-        end = now + datetime.timedelta(0, 3600)
-        d = end.date()
-        plandict['enddate'] = '%04i-%02i-%02i' % (d.year, d.month, d.day)
-        t = end.time()
-        plandict['endtime'] = t.strftime('%H:%M:%S')
-    
-        # Decide the pass.
-        goodseeing = plandict['seeing']<1.3
-        photometric = plandict['transparency']>0.95
-    
-        if goodseeing and photometric:
-            passnum = 1
-        elif goodseeing or photometric:
-            passunm = 2
-        else:
-            passnum = 3
-        plandict['pass'] = passnum
-    
-        plandict['portion'] = portion
-        
-        print('Replan command:')
-        print()
-        print('python2.7 nightlystrategy.py --seeg %(seeing).3f --seer %(seeing).3f --seez %(seeing).3f --sbg %(sbg).3f --sbr %(sbr).3f --sbz %(sbz).3f --transparency %(transparency).3f --start-date %(startdate)s --start-time %(starttime)s --end-date %(enddate)s --end-time %(endtime)s --date %(startdate)s --portion %(portion)f --pass %(pass)i' % plandict) 
-        print()
-    
-        # Gather all the QAplots into a single pdf.
-        qafile = 'qa-%i.pdf' % expnum
-        pnglist = sorted(glob('qa-%i-??.png' % expnum))
-        cmd = 'convert {} {}'.format(' '.join(pnglist), qafile)
-        print('Writing out {}'.format(qafile))
-        print(cmd)
-        os.system(cmd)
-        [os.remove(png) for png in pnglist]
+
+        process_image(fn, rawext, gvs, sfd, opt)
 
