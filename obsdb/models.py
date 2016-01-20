cc22dd92af240496385c648c6c3fafa609ad5afd
Dustin Lang <dstndstn@gmail.com>
2016-01-20 07:40:11 -0500

    set default camera

--- obsdb/models.py
+++ obsdb/models.py
@@ -2,9 +2,11 @@ from django.db import models
 import numpy as np
 
 class MeasuredCCD(models.Model):
-    camera = models.CharField(max_length=32,
-                              choices=(('decam','DECam'),
-                                       ('mosaic3','Mosaic3')))
+    camera = models.CharField(
+        max_length=32,
+        choices=(('decam','DECam'),
+                 ('mosaic3','Mosaic3')),
+        default='decam')
     filename = models.CharField(max_length=1024)
     extension = models.CharField(max_length=32)
     expnum = models.IntegerField(default=0)
