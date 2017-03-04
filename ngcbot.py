52946473f0deb4cec3aea77d03b402ad0faa76e4
decalsobserver <decals@legacysurvey.org>
2017-03-04 06:08:18 -0300

    resample new image to SDSS/DEcaLS frame

--- ngcbot.py
+++ ngcbot.py
@@ -54,6 +54,42 @@ def main():
     bot.run()
     return 0
 
+
+def sdss_rgb(rimgs, bands, scales=None,
+             m = 0.02):
+    import numpy as np
+    rgbscales = {'u': 1.5, #1.0,
+                 'g': 2.5,
+                 'r': 1.5,
+                 'i': 1.0,
+                 'z': 0.4, #0.3
+                 }
+    if scales is not None:
+        rgbscales.update(scales)
+        
+    b,g,r = [rimg * rgbscales[b] for rimg,b in zip(rimgs, bands)]
+    r = np.maximum(0, r + m)
+    g = np.maximum(0, g + m)
+    b = np.maximum(0, b + m)
+    I = (r+g+b)/3.
+    Q = 20
+    fI = np.arcsinh(Q * I) / np.sqrt(Q)
+    I += (I == 0.) * 1e-6
+    R = fI * r / I
+    G = fI * g / I
+    B = fI * b / I
+    # maxrgb = reduce(np.maximum, [R,G,B])
+    # J = (maxrgb > 1.)
+    # R[J] = R[J]/maxrgb[J]
+    # G[J] = G[J]/maxrgb[J]
+    # B[J] = B[J]/maxrgb[J]
+    rgb = np.dstack((R,G,B))
+    rgb = np.clip(rgb, 0, 1)
+    return rgb
+                
+
+
+
 class NgcBot(NewFileWatcher):
     def __init__(self, imagedir, nom, opt):
         super(NgcBot, self).__init__(imagedir, backlog=False)
@@ -136,9 +172,15 @@ class NgcBot(NewFileWatcher):
 
             zpt = self.nom.zeropoint(band, ext)
             print('Nominal zeropoint:', zpt)
-            
+
+
             raw,hdr = meas.read_raw(F, ext)
             wcs = meas.get_wcs(hdr)
+
+
+
+            print('raw shape:', raw.shape)
+            print('WCS shape:', wcs.shape)
 
             median,hi = np.percentile(raw.ravel(), [50, 99])
             ok,x,y = wcs.radec2pixelxy(r, d)
@@ -162,7 +204,8 @@ class NgcBot(NewFileWatcher):
             # plt.title(tt)
             # ps.savefig()
 
-            H,W = wcs.shape
+            #H,W = wcs.shape
+            H,W = raw.shape
             xl,xh = int(np.clip(x-r, 0, W-1)), int(np.clip(x+r, 0, W-1))
             yl,yh = int(np.clip(y-r, 0, H-1)), int(np.clip(y+r, 0, H-1))
             if xl == xh or yl == yh:
@@ -173,8 +216,36 @@ class NgcBot(NewFileWatcher):
                 continue
             print('Subimage size:', subimg.shape)
 
+            print('subimage:', xl, yl, xh-xl, yh-yl)
+            print('x range', xl, xh, 'y range', yl,yh)
+
             subwcs = wcs.get_subimage(xl, yl, xh-xl, yh-yl)
             print('Subwcs:', subwcs.shape)
+
+
+            # Has the copilot measured this exposure yet?
+
+            meas.ext = ext
+            M = meas.run(n_fwhm=1, verbose=False)
+            print('Measured:', M.keys())
+
+            dx = int(np.round(M['dx']))
+            dy = int(np.round(M['dy']))
+            print('DX,DY', dx,dy)
+
+            aff = M['affine']
+            x = (xl+xh)/2
+            y = (yl+yh)/2
+            print('x,y', x,y)
+            print('Affine correction terms:', aff)
+            adx = x - aff[0]
+            ady = y - aff[1]
+            corrx = aff[2] + aff[3] * adx + aff[4] * ady - adx
+            corry = aff[5] + aff[6] * adx + aff[7] * ady - ady
+            print('Affine correction', corrx, corry)
+
+            subimg2 = raw[(yl-dy):(yh-dy), (xl-dx):(xh-dx)]
+            
 
             lo,hi = np.percentile(subimg.ravel(), [50, 99.5])
 
@@ -232,20 +303,6 @@ class NgcBot(NewFileWatcher):
                 jpg = np.flipud(jpg)
 
                 print('Got jpg', jpg.shape)
-                #jpegs.append((layer,jpg))
-
-                # url = urlpat % ('fits', obj.ra, obj.dec, subwcs.pixel_scale() * scale, rw, rh, layer)
-                # print('URL:', url)
-                # r = requests.get(url)
-                # ftmp = tempfile.NamedTemporaryFile()
-                # ftmp.write(r.content)
-                # ftmp.flush()
-                # fits,hdr = fitsio.read(ftmp.name, header=True)
-                # ftmp.close()
-                # print('Got:', fits.shape)
-                # thiswcs = Tan(hdr)
-                # print('Got WCS:', thiswcs)
-                # print('Bands:', hdr['BANDS'])
 
                 pixsc = subwcs.pixel_scale() * scale / 3600.
                 thiswcs = Tan(*[float(x) for x in
@@ -259,61 +316,95 @@ class NgcBot(NewFileWatcher):
                 except:
                     continue
 
-                # print('subwcs:', subwcs)
-                # print('thiswcs:', thiswcs)
+                print('subwcs:', subwcs)
+                print('thiswcs:', thiswcs)
                 # 
                 # print('subwcs RA,Dec bounds:', subwcs.radec_bounds())
                 # print('thiswcs RA,Dec bounds:', thiswcs.radec_bounds())
                 # 
-                # print('Yo', Yo.min(), Yo.max())
-                # print('Xo', Xo.min(), Xo.max())
-                # print('Yi', Yi.min(), Yi.max())
-                # print('Xi', Xi.min(), Xi.max())
+                print('Yo', Yo.min(), Yo.max())
+                print('Xo', Xo.min(), Xo.max())
+                print('Yi', Yi.min(), Yi.max())
+                print('Xi', Xi.min(), Xi.max())
 
                 resamp = np.zeros((sh,sw,3), dtype=jpg.dtype)
-                #print('Resamp:', resamp.shape, resamp.dtype)
 
-                # print('resamp shape', resamp.shape)
-                # print('vs subwcs.shape', subwcs.shape)
-                # print('jpg shape', jpg.shape)
-                # print('vs thiswcs.shape',thiswcs.shape)
+                print('resamp shape', resamp.shape)
+                print('vs subwcs.shape', subwcs.shape)
+                print('jpg shape', jpg.shape)
+                print('vs thiswcs.shape',thiswcs.shape)
 
                 for k in range(3):
                     resamp[Yo,Xo,k] = jpg[Yi,Xi,k]
 
-                jpegs.append((layer, jpg))
+                #jpegs.append((layer + ' resamp', resamp))
+                #jpegs.append((layer + ' jpg', jpg))
 
-                jpegs.append((layer, resamp))
-                
-            # Has the copilot measured this exposure yet?
+                # resamp2 = np.zeros((rh,rw), dtype=subimg.dtype)
+                # try:
+                #     Yo,Xo,Yi,Xi,rims = resample_with_wcs(thiswcs, subwcs)
+                # except:
+                #     continue
+                # resamp2[Yo,Xo] = subimg[Yi,Xi]
+                # 
+                # nl = nlmap(resamp2)
+                # v = np.clip((nl - nlmap(lo)) / (nl.max() - nlmap(lo)), 0., 1.)
+                # rgb = matplotlib.cm.hot(v)
+                # jpegs.append(('resamp2 '+layer, rgb))
 
-            meas.ext = ext
-            M = meas.run(n_fwhm=1, verbose=False)
-            
-            #print('Measured:', M)
+                ### Shift the 'subwcs' to account for astrometric offset
 
-            dx = int(np.round(M['dx']))
-            dy = int(np.round(M['dy']))
-            print('DX,Dy', dx,dy)
+                cx,cy = subwcs.get_crpix()
+                subwcs.set_crpix((cx - dx, cy - dy))
 
-            aff = M['affine']
-            x = (xl+xh)/2
-            y = (yl+yh)/2
-            print('x,y', x,y)
-            print('Affine correction terms:', aff)
-            adx = x - aff[0]
-            ady = y - aff[1]
+                resamp3 = np.zeros((rh,rw), dtype=subimg.dtype)
+                try:
+                    Yo,Xo,Yi,Xi,rims = resample_with_wcs(thiswcs, subwcs)
+                except:
+                    continue
+                resamp3[Yo,Xo] = subimg[Yi,Xi]
 
-            corrx = aff[2] + aff[3] * adx + aff[4] * ady - adx
-            corry = aff[5] + aff[6] * adx + aff[7] * ady - ady
+                nl = nlmap(resamp3)
+                v = np.clip((nl - nlmap(lo)) / (nl.max() - nlmap(lo)), 0., 1.)
+                rgb = matplotlib.cm.hot(v)
+                jpegs.append(('resamp3 '+layer, rgb))
 
-            print('Affine correction', corrx, corry)
 
-            #subimg2 = raw[(yl+dy):(yh+dy), (xl+dx):(xh+dx)]
-            subimg2 = raw[(yl-dy):(yh-dy), (xl-dx):(xh-dx)]
-            print('subimg2:', subimg2.shape)
+                url = urlpat % ('fits', obj.ra, obj.dec, subwcs.pixel_scale() * scale, rw, rh, layer)
+                print('URL:', url)
+                r = requests.get(url)
 
-            # jpegs.append(('dx', subimg2))
+                # ftmp = tempfile.NamedTemporaryFile()
+                # ftmp.write(r.content)
+                # ftmp.flush()
+                # fits,hdr = fitsio.read(ftmp.name, header=True)
+                # ftmp.close()
+                f,tmpfn = tempfile.mkstemp(suffix='.fits')
+                os.write(f, r.content)
+                os.close(f)
+                fits,hdr = fitsio.read(tmpfn, header=True)
+                print('Wrote FITS to', tmpfn)
+
+                print('Got:', fits.shape)
+                fwcs = Tan(hdr)
+                # print('Got WCS:', thiswcs)
+                print('Bands:', hdr['BANDS'])
+                print('Guessed WCS for JPEG:', thiswcs)
+                print('WCS from FITS:', fwcs)
+
+                N,ww,hh = fits.shape
+                imgs = [fits[n,:,:] for n in range(N)]
+
+                bands = hdr['BANDS'].strip()
+                S = 0.5
+                rgb = sdss_rgb(imgs, bands,
+                               #scales=dict(g=6.0, r=3.4, z=2.2),
+                               scales=dict(g=6.0*S, r=3.4*S, z=2.2*S),
+                               m=0)
+                               #m=0.03)
+                jpegs.append((layer + ' fits', rgb))
+
+
 
             tt = '%s in %s' % (obj.name, extname)
             cutouts.append((tt, nlmap(subimg), nlmap(lo), None, nlmap(subimg2), jpegs))
@@ -343,27 +434,21 @@ class NgcBot(NewFileWatcher):
 
         plt.clf()
         NC = len(cutouts)
-        NR = 6
+        NR = 5
         for i,(tt,img,lo,hi,img2, jpegs) in enumerate(cutouts):
             plt.subplot(NR, NC, i+1)
-            plt.imshow(img, interpolation='nearest', origin='lower',
-                       vmin=lo, vmax=hi, cmap='hot')
-            plt.title(tt)
-            plt.xticks([])
-            plt.yticks([])
-
-            plt.subplot(NR, NC, NC + i+1)
             plt.imshow(img2, interpolation='nearest', origin='lower',
                        vmin=lo, vmax=hi, cmap='hot')
             plt.title(tt)
             plt.xticks([])
             plt.yticks([])
 
-            for j,(layer,pix) in enumerate(jpegs):
-                plt.subplot(NR, NC, i+1 + (j+1+1)*NC)
-                #print('pix:', pix.shape, pix.dtype)
+            print(len(jpegs), 'jpegs')
+
+            for j,(layer,pix) in enumerate(jpegs[:NR-1]):
+                plt.subplot(NR, NC, i+1 + (j+1)*NC)
                 plt.imshow(pix, interpolation='nearest', origin='lower')
-                #plt.title(layer)
+                plt.title(layer, fontsize=8)
                 plt.xticks([])
                 plt.yticks([])
         plt.suptitle('Exposure %i: %s band' % (primhdr['EXPNUM'], band))
